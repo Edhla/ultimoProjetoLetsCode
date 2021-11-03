62c941f4ff86ca7d292e07c4a54c2bbd8daeb1c3
unknown <camaraedla@gmail.com>
2021-11-03 00:03:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import {createElement} from './createElement.js'
+import {exploreObject} from './exploreObject.js'
+import {getEndpoint} from './getEndpoint.js'
+import {inputListener} from './inputListener.js'
+
+// const fetchObject = getEndpoint('rj', 'rio-de-janeiro')
+inputListener()
+// const fetchObject = getEndpoint('sp', 'sao-paulo')
+// const newObject = exploreObject(fetchObject)
+// createElement(newObject)
